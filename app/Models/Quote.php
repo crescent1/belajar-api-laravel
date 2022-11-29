cd08e3d87f00f3d3c6fbc74d12a7ea5f46b67a09
Syaifudin Zuhri <szuhri1@gmail.com>
2022-11-29 07:27:31 +0700

    buat migration, seeder, model, factory

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Quote extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'text',
+        'author'
+    ];
 }
---
+++ database/seeders/QuoteSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Quote;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class QuoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Quote::factory()->count(30)->create();
+    }
+}
