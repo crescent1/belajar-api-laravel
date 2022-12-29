1dbc203a26041c887896ea08e6677719bf4531e9
Syaifudin Zuhri <szuhri1@gmail.com>
2022-12-29 10:37:19 +0700

    buat controller untuk auth

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiAuthController extends Controller
+{
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/quotes', QuoteController::class);
+});
 
-Route::apiResource('/quotes', QuoteController::class);
+
